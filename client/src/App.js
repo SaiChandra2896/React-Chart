c88a35a3233bd0e8dbe26417603f3db74517df18
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-23 18:20:30 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,62 @@
-import React,{useState, useEffect} from 'react';
-import Chart from './components/Chart';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Chart from "./components/Chart";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
       {
-        label: 'RupeeValue',
+        label: "RupeeValue",
         data: [],
-        backgroundColor:''
-      }
-    ]
+        backgroundColor: "",
+      },
+    ],
   });
   const [dataState, setDataState] = useState(false);
   const [response, setResponse] = useState([]);
   const [errors, setErrors] = useState({});
 
-  useEffect(() =>{
+  useEffect(() => {
     getChartData();
-  },[]);
+  }, []);
 
-  const getChartData = async() =>{
+  const getChartData = async () => {
     try {
-      const resData = await axios.get('/api/chart');
+      const resData = await axios.get("/api/chart");
       const data = resData.data;
 
       let dates = [];
       let values = [];
-      data.forEach(e => dates.push(e.Date));
-      data.forEach(e => values.push(parseInt(e.Price,10)))
+      data.forEach((e) => dates.push(e.Date));
+      data.forEach((e) => values.push(parseInt(e.Price, 10)));
 
       setResponse(resData);
       setChartData({
         labels: dates,
         datasets: [
           {
-            label: 'RupeeValue',
+            label: "RupeeValue",
             data: values,
-            backgroundColor: 'rgba(255, 99, 132, 0.6)' 
-          }
-        ]
+            backgroundColor: "rgba(255, 99, 132, 0.6)",
+          },
+        ],
       });
       setDataState(true);
-      
     } catch (err) {
       setErrors(err);
     }
-  }
+  };
   return (
     <div className="container mt-4">
       <h4 className="display-4 text-center mb-4">
-          <i className="fab fa-react"></i> React Charts
+        <i className="fab fa-react"></i> React Charts
       </h4>
-      {dataState && <Chart data={chartData} title='Doller to INR plot'/>}
+      {dataState && <Chart data={chartData} title="Doller to INR plot" />}
+      <button>button</button>
     </div>
   );
-}
+};
 
 export default App;
